ec58055dc3f6bf944daf34071f67d7fc66e780cb
Kyi <khantkhant528@gmail.com>
2024-10-15 22:56:35 +0700

    learning tab and section from tutorial

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,26 @@
-
+import aboutImg from "./Header/react-core-concepts.png";
+import Examples from "./Examples.jsx";
 
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>Learn more about our mission and values.</p>
-    </div>
+    <>
+      <div className="banner">
+        <h1>About Me!</h1>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-2">
+        <div className="flex flex-col items-center bg-white p-4 w-full md:w-1/3">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Odio, adipisci debitis hic ut nostrum dicta neque tempore deleniti ullam illum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Odio, adipisci debitis hic ut nostrum dicta neque tempore deleniti ullam illum. </p>
+        </div>
+        <div className="flex flex-col items-center bg-white p-4 w-full lg:w-1/2">
+          <span><img src={aboutImg} alt="Stylized atom" /> </span>
+        </div>
+      </div>
+
+      <Examples />
+    </>
   )
 };
 
--- src/data.jsx
+++ src/data.jsx
@@ -6,3 +6,19 @@ export const testimonials = [
     { name: "Lorem ipsum", text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit." },
     { name: "Lorem ipsum", text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit." },
 ];
+
+
+export const EXAMPLES = {
+    water: {
+        title: 'Components',
+        description:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        code: ``,
+    },
+    sugar: {
+        title: 'JSX',
+        description:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit.Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        code: ``,
+    },
+};
