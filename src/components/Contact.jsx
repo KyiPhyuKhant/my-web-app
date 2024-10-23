2b8ef0c2ae6b5b006b2743049d86b18def77d187
Kyi <khantkhant528@gmail.com>
2024-10-23 23:37:54 +0700

    Deriving Computed Values From Other Computed Values

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,14 @@ import Player from './Player.jsx';
 import GameBoard from "./GameBoard.jsx";
 import { useState } from "react";
 import Log from "./Log.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
+
+const intialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
 
 function deriveActivePlayer(gameTurns){
   let currentPlayer = 'X';
@@ -20,6 +27,30 @@ const Contact = () => {
   // const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = intialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+      if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+        winner = firstSquareSymbol;
+      }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
@@ -50,7 +81,8 @@ const Contact = () => {
             symbol="O"
             isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </>
