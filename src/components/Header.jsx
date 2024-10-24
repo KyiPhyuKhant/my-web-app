64dd48e53f4af7195ad7144f584dfcf000c5b151
Kyi <khantkhant528@gmail.com>
2024-10-24 19:55:02 +0700

    added a Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,10 @@
-import logoImg from "../../components/Header/react-core-concepts.png";
-import { Link } from "react-router-dom";
-import "../../App.css";
-import './Header.css';
+import logo from '../assets/investment-calculator-logo.png';
 
 export default function Header() {
   return (
-    <div className="mainheader bg-black shadow-md p-4 flex items-center justify-between">
-      <div className="mainlogo">
-        <img src={logoImg} alt="Logo" className="h-12 w-auto" />
-      </div>
-      <div className="menu">
-        <nav>
-          <ul className="flex space-x-6">
-            <li>
-              <Link to="/" className="text-white-700 hover:text-blue-600 transition">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className="text-white-700 hover:text-blue-600 transition">
-                About Me
-              </Link>
-            </li>
-            <li>
-              <Link to="/game" className="text-white-700 hover:text-blue-600 transition">
-                Game
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+    <header id='header'>
+      <img src={logo} alt="Logo showing a money bag" />
+      <h1>Investment Calculator</h1>
+    </header>
   );
 }
