72209e9753daf0b4e2b492cf2a8dea398262bce1
Kyi <khantkhant528@gmail.com>
2024-10-24 21:41:37 +0700

    Output Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
 import Results from './components/Results.jsx';
@@ -11,21 +12,27 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
+
   return (
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
-  )
+  );
 }
 
 export default App;
