a9f1995a8e3a033a440791745debed493002369b
Kyi <khantkhant528@gmail.com>
2024-10-26 23:33:58 +0700

    splitted css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header"; // Adjust the path as needed
-import Footer from "./components/Footer"; // Adjust the path as needed
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import "./App.css";
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
+      <main>
+        <AuthInputs />
+      </main>
+    </>
   );
 }
-
-export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,12 @@
-import logoImg from "../../components/Header/react-core-concepts.png";
-import { Link } from "react-router-dom";
-import "../../App.css";
+import logo from '../assets/logo.png';
 import './Header.css';
 
 export default function Header() {
   return (
-    <div className="mainheader bg-black shadow-md p-4 flex items-center justify-between">
-      <div className="mainlogo">
-        <img src={logoImg} alt="Logo" className="h-12 w-auto" />
-      </div>
-      <div className="menu">
-        <nav>
-          <ul className="flex space-x-6">
-            <li>
-              <Link to="/" className="text-white-700 hover:text-blue-600 transition">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className="text-white-700 hover:text-blue-600 transition">
-                About Me
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" className="text-white-700 hover:text-blue-600 transition">
-                Contact Me
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+    <header>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+    </header>
   );
 }
