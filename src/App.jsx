5b2aca0304c7b45dfa5c1ebb0bdc34d6a1e5cb80
Kyi <khantkhant528@gmail.com>
2024-10-30 23:24:08 +0700

    added challenges to demo project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}/>  
+        <TimerChallenge title="Easy" targetTime={2}/>  
+        <TimerChallenge title="Easy" targetTime={3}/>  
+        <TimerChallenge title="Easy" targetTime={4}/>  
+      </div>
     </>
   );
 }
