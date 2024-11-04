0be4558ad2ebf7eb99a20ec7edc225056e6d3565
Kyi <khantkhant528@gmail.com>
2024-11-04 18:08:37 +0700

    starting project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header"; // Adjust the path as needed
-import Footer from "./components/Footer"; // Adjust the path as needed
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import "./App.css";
-
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </>
   );
 }
 
